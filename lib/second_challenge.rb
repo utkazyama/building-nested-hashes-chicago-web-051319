e797c22022e560d445174baa2bd6b8332970240d
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-20 04:06:12 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,12 +2,12 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriarch => {:name => "Lord Montague",:age => "53"},
-      :matriarch => {:name => ""},
-      :hero => {},
+      :matriarch => {:name => "Lady Montague", :age => "54"},
+      :hero => {:name => "Romeo", :age => "15", :status => "alive"},
       :hero_friends => []
       }, 
     :capulet => {
-      :patriarch => {},
+      :patriarch => {:name => ""},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
