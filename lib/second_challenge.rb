aa2b25f2004553fef98e1584e0ef52e6a7171cd0
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-20 03:51:15 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -10,8 +10,9 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :heroine => {},
-      :heroine_friends => {}
+      :heroine_friends => []
       }
+     }
 
   
 end
