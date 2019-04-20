ff1fe6bb297b3807c7c86974d138545f594fcd9b
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-20 04:28:08 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,4 +1,5 @@
- def epic_tragedy = {
+ def third_challenge
+   epic_tragedy = {
     :montague => {
        :patriarch => {name: "Lord Montague", age: "53"},
        :matriarch => {name: "Lady Montague", age: "54"},
