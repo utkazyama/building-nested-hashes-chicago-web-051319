9e900411571070db9e42792d87979f54b319a985
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-20 03:46:07 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,7 +2,8 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
-      :patriarch
+      :patriarch => {},
+      
       }, 
     :capulet => {
       #your key/value pairs here!
