89ee987c4a43543a639e92ff05934109fe921d70
Yuta Katsuyama <ykatsuyama@id.iit.edu>
2019-04-20 04:11:42 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,7 +4,7 @@ def second_challenge
       :patriarch => {:name => "Lord Montague",:age => "53"},
       :matriarch => {:name => "Lady Montague", :age => "54"},
       :hero => {:name => "Romeo", :age => "15", :status => "alive"},
-      :hero_friends => [{:name => ""}]
+      :hero_friends => [{:name => "Benvolio", :age => "17", :attitude => "worried"},{:name => }]
       }, 
     :capulet => {
       :patriarch => {:name => "Lord Capulet", :age => "50"},
